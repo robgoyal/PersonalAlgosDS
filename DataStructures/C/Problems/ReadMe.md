003229a8f3182872e3659cc422c203a239711408
Robin Goyal <goyal.rob@gmail.com>
2017-05-29 19:35:36 -0400

    Added stack header file

--- DataStructures/C/Problems/ReadMe.md
+++
@@ -1,3 +0,0 @@
-# Linked List Problems
-
-A storage space for [problems](http://cslibrary.stanford.edu/105/LinkedListProblems.pdf) involving linked lists. This directory includes copies of the linked list structure that I created so the .c and .h files can be included in the solutions to the problems. 
---
+++ DataStructures/C/Stack/stack.h
@@ -0,0 +1,35 @@
+/* Name: stack.h
+   Author: Robin Goyal
+   Last-Modified: May 29, 2017
+   Purpose: Define stack struct and prototypes. Implemented 
+            on top of linked list
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#ifndef STACK_H_ /* Include guard */
+#define STACK_H_
+
+typedef struct stack {
+    int data;
+    struct stack* next;
+} node;
+
+// Creates an empty stack
+node* create();
+
+// Pushes a value onto the stack
+void push(node** head, int val);
+
+// Return top value of stack
+int top(node** head);
+
+// Check if stack is empty 
+bool isEmpty(node** head);
+
+// Remove and return the top value of the stack
+int pop(node** head);
+
+#endif // STACK_H_ 
